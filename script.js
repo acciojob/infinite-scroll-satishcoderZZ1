678d20005a5b527373cb888f0e353705cf089d35
satishcoderZZ1 <167805933+satishcoderZZ1@users.noreply.github.com>
2025-06-15 20:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+const list=document.getElementById("infi-list");
+let itemCount=0;
 
+function addItems(count){
+	for(let i=0;i<count;i++){
+		const li=document.createElement("li");
+		li.textContent=`List Item ${++itemCount}`;
+		list.appendChild(li);
+	}
+}
+addItems(10);
+
+window.addEventListener("scroll", () =>{
+	if(window.innerHeight + window.scrolly >= document.body.offsetHeight){
+		addItems(2);
+	}
+})
